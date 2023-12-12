f696dc3d41d809cebe0f243c431eff56e2889bb4
DigitallyIntrinsic <139934612+DigitallyIntrinsic@users.noreply.github.com>
2023-12-12 11:05:32 -0700

    Started thoughts js

---
+++ controllers/thoughtController.js
@@ -0,0 +1,14 @@
+// Imports
+const { User, Thought } = require("../models");
+
+// Get all thoughts
+const thoughtController = {
+  async getThoughts(req, res) {
+    try {
+      const thoughts = await Thought.find();
+      return res.status(200).json(thoughts);
+    } catch (err) {
+      console.log(err);
+      return res.status(500).json(err);
+    }
+  },
