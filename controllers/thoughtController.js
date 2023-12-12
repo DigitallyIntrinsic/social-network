61c5b8008983015a2da96eece191bbb084496292
DigitallyIntrinsic <139934612+DigitallyIntrinsic@users.noreply.github.com>
2023-12-12 11:06:00 -0700

    continued thoughts js

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -12,3 +12,21 @@ const thoughtController = {
       return res.status(500).json(err);
     }
   },
+
+  // Get single thought
+  async getThought(req, res) {
+    try {
+      const thought = await Thought.findOne({ _id: req.params.thoughtId });
+
+      if (!thought) {
+        return res.status(404).json({ message: "No thought with that ID" });
+      }
+
+      return res.status(200).json(thought);
+    } catch (err) {
+      console.log(err);
+      return res.status(500).json(err);
+    }
+  },
+
+  
