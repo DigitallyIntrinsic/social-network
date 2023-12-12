d2f5292e176d4f8ffda6921e93e1b40e3526a26a
DigitallyIntrinsic <139934612+DigitallyIntrinsic@users.noreply.github.com>
2023-12-12 11:11:52 -0700

    started user controller

---
+++ controllers/userController.js
@@ -0,0 +1,18 @@
+// Imports
+const { User, Thought } = require("../models");
+
+// Get all users
+const userController = {
+  async getUsers(req, res) {
+    try {
+      const users = await User.find()
+
+        .populate({ path: "thoughts", select: "-__v" })
+        .populate({ path: "friends", select: "-__v" });
+
+      return res.status(200).json(users);
+    } catch (err) {
+      console.log(err);
+      return res.status(500).json(err);
+    }
+  },
