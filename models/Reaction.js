5cace69faa5bb352c14e540dc40ab04379bcfb22
DigitallyIntrinsic <139934612+DigitallyIntrinsic@users.noreply.github.com>
2023-12-12 11:25:25 -0700

    created reaction js in models folder

---
+++ models/Reaction.js
@@ -0,0 +1,26 @@
+// Imports
+const { Schema, Types } = require("mongoose");
+
+// This will not be a model, but will be used as sub-doc in the Thought model
+const reactionSchema = new Schema({
+  reactionId: {
+    type: Schema.Types.ObjectId,
+    default: () => new Types.ObjectId(),
+  },
+  reactionBody: {
+    type: String,
+    required: true,
+    maxlength: 280,
+  },
+  username: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+// Exports
+module.exports = reactionSchema;
