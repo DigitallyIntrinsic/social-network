9183328dd61a8b07350785f4e6d46fa79ff78185
DigitallyIntrinsic <139934612+DigitallyIntrinsic@users.noreply.github.com>
2023-12-12 10:59:11 -0700

    Added connection.js in config

---
+++ config/connection.js
@@ -0,0 +1,12 @@
+// Imports
+const { connect, connection } = require("mongoose");
+
+// Creates database
+const connectionString =
+  process.env.MONGODB_URI || "mongodb://localhost:27017/studentsDB";
+
+// Connects Mongoose and MongoDB
+connect(connectionString);
+
+// Exports
+module.exports = connection;
